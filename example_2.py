6bb699f75f636cd5f4d1a73c5e426ab270ba2e8a
M-Saeb <Mohammed.saeb98@hotmail.com>
2024-08-23 18:11:23 +0200

    update example_2.py

--- example_2.py
+++ example_2.py
@@ -24,25 +24,25 @@ def animation_plot(frame_number):
     current_applied_changes = np.zeros_like(INPUT_SIGNAL)
     
     # Accumulate the reconstructed signal only up to the current iteration `i`
-    for j in range(frame_number+1):
+    for j in range(frame_number):
         current_applied_changes += coefficients[j] * DICTIONARY[indices[j]]
     
     current_signal = INPUT_SIGNAL + current_applied_changes  # Accumulate the reconstructed signal
     
-    figure1.plot(current_signal, color='orange', linestyle='dashed', label='Reconstructed Signal')
     figure1.plot(INPUT_SIGNAL, color='grey', label='Input Signal')
+    figure1.plot(current_signal, color='orange', linestyle='dashed', label='Reconstructed Signal')
     figure1.plot(EXPECTED_SIGNAL, color='blue', linestyle='dotted', label='Expected Signal')
     figure1.set_xlim([0, len(INPUT_SIGNAL)])
     figure1.set_ylim([min(EXPECTED_SIGNAL)-1, max(EXPECTED_SIGNAL)+1])
     figure1.set_title("Reconstructed Signal after {} Iterations".format(frame_number))
     figure1.legend()
     
-
-    figure2.stem(
-        indices[:frame_number+1], # [X-AXIS]
-        coefficients[:frame_number+1], # [Y-AXIS]
-        linefmt='purple', markerfmt='x', basefmt=' '
-    )
+    if frame_number > 0: # This condition is to not plot any coefficient on the first frame
+        figure2.stem(
+            indices[:frame_number], # [X-AXIS]
+            coefficients[:frame_number], # [Y-AXIS]
+            linefmt='purple', markerfmt='x', basefmt=' '
+        )
     figure2.set_xlim([0, len(DICTIONARY)])
     figure2.set_title("Coefficients after {} Iterations".format(frame_number))
     figure2.set_xlabel("Element of the basis")
